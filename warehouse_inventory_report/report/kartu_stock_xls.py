5f76c9314cae14a391b81a8ea7753abdb129023b
kev00kev <79142712+kev00kev@users.noreply.github.com>
2023-06-21 10:10:22 +0700

    Kartu Stock
    Kartu Stock

--- warehouse_inventory_report/report/kartu_stock_xls.py
+++ warehouse_inventory_report/report/kartu_stock_xls.py
@@ -1,37 +1,70 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models
-from datetime import datetime
+from datetime import date
 
 class KartuStockXlsx(models.AbstractModel):
     _name = 'report.warehouse_inventory_report.report_kartu_stock_xls'
     _inherit = 'report.report_xlsx.abstract'
-
     def generate_xlsx_report(self, workbook, data, stock):
-        bold = workbook.add_format({'bold': True})
-        bold_center = workbook.add_format({'bold': True, 'align': 'center'})
-        sheet = workbook.add_worksheet('Kartu Stock')
-        sheet.set_column('A:A', 15)
-        sheet.set_column('B:B', 15)
-        sheet.set_column('C:C', 30)
-        sheet.set_column('D:D', 15)
-        sheet.set_column('E:E', 15)
-        sheet.set_column('F:F', 15)
-        sheet.set_column('G:G', 15)
-        sheet.set_column('H:H', 15)
-        sheet.set_column('I:I', 15)
-        sheet.set_column('J:J', 30)
-        sheet.set_column('K:K', 30)
-        sheet.set_column('L:L', 30)
+        today = date.today()
+        dt = 'Tanggal Print : ' + str(today.strftime("%d %B %Y"))
+        bold = workbook.add_format({'bold': True, 'border': 2})
+        bold_bg = workbook.add_format({'bold': True, 'border': 1, 'bg_color': 'yellow'})
+        bold_center = workbook.add_format({'bold': True, 'align': 'center', 'border': 2, 'bg_color': 'yellow'})
 
-        sheet.write(0, 0, 'PT. TRIYUDA TOPHERINDO NUSANTARA', bold)
-        sheet.write(1, 0, 'NAMA BARANG: ', bold)
-        sheet.write(2, 0, 'SATUAN: ', bold)
-        sheet.write(3, 0, 'NO KARTU: ', bold)
+        for i in data['product']:
+            row = 9
+            saldo = 0
+            sheet = workbook.add_worksheet(i[1])
+            sheet.set_column('A:A', 15)
+            sheet.set_column('B:B', 45)
+            sheet.set_column('C:C', 45)
+            sheet.set_column('D:D', 15)
+            sheet.set_column('E:E', 15)
+            sheet.set_column('F:F', 15)
+            sheet.set_column('G:G', 15)
+            sheet.set_column('H:H', 15)
+            sheet.set_column('I:I', 15)
+            sheet.set_column('J:J', 30)
+            sheet.set_column('K:K', 30)
+            sheet.set_column('L:L', 30)
 
-        sheet.write(5, 0, 'TGL', bold_center)
-        sheet.write(5, 1, 'NO BUKTI', bold_center)
-        sheet.write(5, 2, 'KETERANGAN', bold_center)
-        sheet.write(5, 3, 'MASUK', bold_center)
-        sheet.write(5, 4, 'KELUAR', bold_center)
-        sheet.write(5, 5, 'SISA', bold_center)
+            sheet.merge_range(0, 0, 0, 5, 'PT. TRIYUDA TOPHERINDO NUSANTARA', bold_bg)
+            sheet.merge_range(1, 0, 1, 5, 'NAMA BARANG : [' + i[0] + '] ' + i[1], bold)
+            sheet.merge_range(2, 0, 2, 5, 'SATUAN : ' + i[2], bold)
+            sheet.merge_range(3, 0, 3, 5, 'NO KARTU : ', bold)
+            sheet.merge_range(4, 0, 4, 5, dt, bold)
+
+            sheet.write(7, 0, 'TGL', bold_center)
+            sheet.write(7, 1, 'NO BUKTI', bold_center)
+            sheet.write(7, 2, 'KETERANGAN', bold_center)
+            sheet.write(7, 3, 'MASUK', bold_center)
+            sheet.write(7, 4, 'KELUAR', bold_center)
+            sheet.write(7, 5, 'SISA', bold_center)
+
+            sheet.write(8, 0, '', bold)
+            sheet.write(8, 1, '', bold)
+            sheet.write(8, 2, 'saldo awal = ' + str(i[3]), bold)
+            sheet.write(8, 3, '', bold)
+            sheet.write(8, 4, '', bold)
+            sheet.write(8, 5, '', bold)
+
+            saldo = i[3]
+            for data_stock in i[4]:
+                sheet.write(row, 0, data_stock[0], bold)
+                sheet.write(row, 1, data_stock[1], bold)
+                sheet.write(row, 2, data_stock[2], bold)
+                if data_stock[3] != 0:
+                    sheet.write(row, 3, data_stock[3], bold)
+                    saldo = saldo + data_stock[3]
+                else:
+                    sheet.write(row, 3, '', bold)
+                if data_stock[4] != 0:
+                    sheet.write(row, 4, data_stock[4], bold)
+                    saldo = saldo - data_stock[4]
+                else:
+                    sheet.write(row, 4, '', bold)
+                sheet.write(row, 5, saldo, bold)
+                row += 1
+
--- warehouse_inventory_report/wizard/kartu_stock.py
+++ warehouse_inventory_report/wizard/kartu_stock.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from datetime import timedelta, date
+import dateutil.parser
 
 class KartuStockWizard(models.TransientModel):
     _name = "kartu.stock.wizard"
@@ -8,16 +10,121 @@ class KartuStockWizard(models.TransientModel):
 
     date_from = fields.Date(string='Start Date', required=True)
     date_to = fields.Date(string='End Date', required=True)
-    kategori_produk = fields.Char(string='Kategori Produk', required=True)
-    produk = fields.Char(string="Produk", required=False, invisible=True)
+    kategori_produk = fields.Many2one('product.category', string='Kategori Produk', domain="[('parent_id', '=', False)]", required=True)
+    produk = fields.Many2many('product.product', string="Produk", required=False, invisible=True)
     all_produk = fields.Boolean(string="All Produk", default=True, )
 
 
 
+    @api.onchange("kategori_produk")
+    def _onchange_kategori_produk(self):
+        for rec in self:
+            return {'domain': {'produk': [('categ_id', '=', rec.kategori_produk.name)]}, }
+
+
     def action_print_kartu(self):
+        nama_product = []
+        location_name = []
+        date_range = []
+        produk = self.env['product.product'].search([
+            ('categ_id', '=', self.kategori_produk.name)
+        ])
+        stock_location_name = self.env['stock.location'].search([
+            ('categ_id', '=', self.kategori_produk.name)
+        ])
+        for rec_stock in stock_location_name:
+            location_name.append(rec_stock['complete_name'])
 
 
-        data = {}
+        def daterange(start_date, end_date):
+            for n in range(int((end_date - start_date).days) + 1):
+                yield start_date + timedelta(n)
+
+        for single_date in daterange(self.date_from, self.date_to):
+            date_range.append(single_date)
+
+
+
+
+
+        if self.all_produk == True:
+            saldo_awal = 0
+            stock_move_range = []
+
+            for i in produk:
+                # Mencari Saldo Awal
+                stock_move = self.env['stock.move'].search([
+                    ('state', '=', 'done'),
+                    ('product_id', 'like', str(i['name']))
+                ])
+                for j in stock_move:
+                    if dateutil.parser.parse(str(j['date'])).date() < self.date_from:
+                        if j['location_id'].complete_name in location_name:
+                            print(j['product_uom_qty'], "KELUAR")
+                            saldo_awal = saldo_awal - (j['product_uom_qty'])
+                        if j['location_dest_id'].complete_name in location_name:
+                            print(j['product_uom_qty'], "MASUK")
+                            saldo_awal = saldo_awal + (j['product_uom_qty'])
+
+                # Mencari Data Berdasarkan Range
+                stock_move_env = self.env['stock.move'].search([
+                    ('state', '=', 'done'),
+                    ('product_id', 'like', str(i['name']))
+                ])
+                for j in stock_move_env:
+                    print(dateutil.parser.parse(str(j['date'])).date())
+                    if dateutil.parser.parse(str(j['date'])).date() in date_range:
+                        if j['location_id'].complete_name in location_name:
+                            stock_move_range.append([dateutil.parser.parse(str(j['date'])).date(), j['picking_id'].name, j['picking_id'].note, j['product_uom_qty'], 0 ])
+                        if j['location_dest_id'].complete_name in location_name:
+                            stock_move_range.append([dateutil.parser.parse(str(j['date'])).date(), j['picking_id'].name, j['picking_id'].note, 0, j['product_uom_qty']])
+
+                nama_product.append([str(i['default_code']), str(i['name']), str(i['uom_id'].name), saldo_awal, stock_move_range])
+                saldo_awal = 0
+                stock_move_range = []
+
+
+        else:
+            saldo_awal = 0
+            stock_move_range = []
+
+            for i in self.produk:
+                # Mencari Saldo Awal
+                stock_move = self.env['stock.move'].search([
+                    ('state', '=', 'done'),
+                    ('product_id', 'like', str(i['name']))
+                ])
+                for j in stock_move:
+                    if dateutil.parser.parse(str(j['date'])).date() < self.date_from:
+                        if j['location_id'].complete_name in location_name:
+                            print(j['product_uom_qty'], "KELUAR")
+                            saldo_awal = saldo_awal - (j['product_uom_qty'])
+                        if j['location_dest_id'].complete_name in location_name:
+                            print(j['product_uom_qty'], "MASUK")
+                            saldo_awal = saldo_awal + (j['product_uom_qty'])
+
+                # Mencari Data Berdasarkan Range
+                stock_move_env = self.env['stock.move'].search([
+                    ('state', '=', 'done'),
+                    ('product_id', 'like', str(i['name']))
+                ])
+                for j in stock_move_env:
+                    print(dateutil.parser.parse(str(j['date'])).date())
+                    if dateutil.parser.parse(str(j['date'])).date() in date_range:
+                        if j['location_id'].complete_name in location_name:
+                            stock_move_range.append([dateutil.parser.parse(str(j['date'])).date(), j['picking_id'].name,
+                                                     j['picking_id'].note, j['product_uom_qty'], 0])
+                        if j['location_dest_id'].complete_name in location_name:
+                            stock_move_range.append([dateutil.parser.parse(str(j['date'])).date(), j['picking_id'].name,
+                                                     j['picking_id'].note, 0, j['product_uom_qty']])
+
+                nama_product.append(
+                    [str(i['default_code']), str(i['name']), str(i['uom_id'].name), saldo_awal, stock_move_range])
+                saldo_awal = 0
+                stock_move_range = []
+
+
+        data = {'product': nama_product}
 
         return self.env.ref('warehouse_inventory_report.report_kartu_stock_xlsx').report_action(self, data=data)
 
