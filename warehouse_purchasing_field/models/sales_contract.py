503b19613c1bd5cd59a69e14f914b4bc349187f8
kev00kev <79142712+kev00kev@users.noreply.github.com>
2023-07-08 08:50:56 +0700

    purchase_field_update
    vendor otomatis

--- warehouse_purchasing_field/models/sales_contract.py
+++ warehouse_purchasing_field/models/sales_contract.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
-
-
 class SalesContractField(models.Model):
-    _inherit = "purchase.order.line"
+    _inherit = 'purchase.order.line'
 
+    sales_contract = fields.Many2one('sales.contract', string='Sales Contract', domain="[('status', '=', True)]")
 
-    sales_contract = fields.Many2one('sales.contract', string='sales contract', domain="[('status', '=', True)]")
+class SalesContractFieldReciept(models.Model):
+    _inherit = 'stock.move'
+
+    note = fields.Char(string='Note')
+    sales_contract = fields.Many2one('sales.contract', string='Sales Contract')
 
