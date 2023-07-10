1894883e9b72c1b792371d326402700d6fbe9dcc
Meyrina Herawati <68460620+rexmey@users.noreply.github.com>
2023-07-10 14:28:44 +0700

    warehouse dan sales
    warehouse dan sales

--- mh_sale_pricelist_tri/__manifest__.py
+++ mh_sale_pricelist_tri/__manifest__.py
@@ -6,14 +6,14 @@
     'version': '14.0.0.0',
     'sequence': 1,
     'category': 'Inventory',
-    'summary': 'Stock Card',
+    'summary': 'Sales Pricelist',
     'description': """
 """,
     'author': 'Meyrina',
     'website': 'http://www.rexmey.com/',
-    'depends': ['sale'],
+    'depends': ['sale','mh_warehouse_tri'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/pricelist_view.xml',
     ],
     'installable': True,
--- mh_sale_pricelist_tri/models/pricelist.py
+++ mh_sale_pricelist_tri/models/pricelist.py
@@ -1,5 +1,25 @@
 from odoo import fields, models, api
 
+class CustomerBasePrice(models.Model):
+    _name = 'customer.base.price'
+    _description = 'Customer Base Price'
+
+    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist')
+    customer_id = fields.Many2one('res.partner', string='Customer')
+    type =fields.Many2one('product.type','Type')
+    bentuk = fields.Selection([('bulat', 'Bulat'), ('kotak', 'Kotak'), ], string='Bentuk', )
+    base_price = fields.Float('Base Price')
+
+class CustomerChangePrice(models.Model):
+    _name = 'customer.change.price'
+    _description = 'Customer Base Price'
+
+    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist')
+    customer_id = fields.Many2one('res.partner', string='Customer')
+    type =fields.Many2one('product.type','Type')
+    bentuk = fields.Selection([('bulat', 'Bulat'), ('kotak', 'Kotak'), ], string='Bentuk', )
+    inc_dec_price = fields.Float('Increase/Decrease Price')
+
 class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
 
@@ -11,9 +31,11 @@ class ProductPricelist(models.Model):
 
     price_method = fields.Selection([
         ('base', 'Update Base price By Customer'),
-        ('inc/dec', 'Increase/Decrease Base Price By Type')
+        ('incdec', 'Increase/Decrease Base Price By Type')
     ], string='Compute Price Method')
 
+    base_price_ids = fields.One2many('customer.base.price', 'pricelist_id', string='Base Price')
+    change_price_ids = fields.One2many('customer.change.price', 'pricelist_id', string='Increase/Decrease Price')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('waiting', 'Waiting Approved'),
